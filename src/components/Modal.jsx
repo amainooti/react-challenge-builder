1ccee484455696fa5c6238a29c30a291214fab61
amainooti <amainooti@gmail.com>
2022-09-29 12:14:44 +0100

    I just dey do rubbish lmao

---
+++ src/components/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Modal() {
+  return (
+      <div className='bg-gray-700 h-[50px] w-[150px] absolute top-4  mt-3 ml-5 text-center '>
+          <p className='text-white  hover:text-green-500'>Wesley (Sudent)</p>
+          <p className='text-white  hover:text-green-500'>Oti (Mentor) </p>
+    </div>
+  )
+}
+
+export default Modal
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Modal from './Modal';
 
 function Navbar() {
+    const [option, setOption] = React.useState(false);
+
+    const optionPanel = option && <Modal />
+
+    const handleOption = () => {
+        setOption( prevState => !prevState )
+    }
+
+
   return (
 
           <div className='md:w-[100%] h-14 '>
-              <nav className='w-auto h-auto flex justify-around align-middle'>
-              <ul className='md:flex justify-around align-middle gap-6 mt-3 hidden'>
-                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><a href= "/">Home</a></li>
-                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/projects">Projects</Link></li>
-                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/progress"> Progress </Link></li>
-                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/timeline"> Timeline </Link></li>
+              <nav className='w-auto gap-12 h-auto flex justify-around align-middle'>
+                  <div>
+                  <h1 className='text-[#21272F] mt-3'><code className='bg-green-700 p-2 text-white before:content-["<"] after:content-["/>"] cursor-pointer' onClick={handleOption}>Zero dulling </code></h1>
+                  {optionPanel}
+                  </div>
+                  <ul className='md:flex justify-around align-middle gap-6 mt-3 hidden'>
+                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><a href= "/">Home</a></li>
+                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/projects">Projects</Link></li>
+                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/progress"> Progress </Link></li>
+                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/timeline"> Timeline </Link></li>
                   </ul>
               </nav>
           </div>
