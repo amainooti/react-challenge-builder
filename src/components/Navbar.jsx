279455451b1c798dd6149e623c259a6227697d8a
amainooti <amainooti@gmail.com>
2022-09-29 11:15:12 +0100

    minor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,11 @@ function Navbar() {
 
           <div className='md:w-[100%] h-14 '>
               <nav className='w-auto h-auto flex justify-around align-middle'>
-
-                  <ul className='md:flex justify-around align-middle gap-6 mt-3 hidden'>
-                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><a href= "/">Home</a></li>
-                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/projects">Projects</Link></li>
-                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/progress"> Progress </Link></li>
-                      <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/timeline"> Timeline </Link></li>
+              <ul className='md:flex justify-around align-middle gap-6 mt-3 hidden'>
+                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><a href= "/">Home</a></li>
+                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/projects">Projects</Link></li>
+                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/progress"> Progress </Link></li>
+                  <li className='text-[#21272F] capitalize hover:border-b-2 border-red-500 '><Link to= "/timeline"> Timeline </Link></li>
                   </ul>
               </nav>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,13 +15,16 @@ function Home() {
   return (
     <>
       <Navbar />
-      <div className='mt-12 flex-col w-[500px] h-[300px] bg-black justify-center align-middle rounded-md'>
+      <div className='mt-12 md:[500px] flex-col w-[300px] lg:w-[500px] h-[300px] bg-black justify-center align-middle rounded-md'>
         <h1 className='text-green-700 pt-20 m-5 italic font-mono'>
-        <span className='text-green-700'>OTI@DESKTOP-UIU2960</span><span className='text-pink-400'> MINGW64</span> <span className='text-yellow-400'>~/OneDrive/Documents/Web dev/builder-tut/client</span> <span className='text-blue-500'>(main)</span>
+          <span className='text-green-700'>
+          OTI@DESKTOP-UIU2960</span>
+          <span className='text-pink-400'> MINGW64</span>
+          <span className='text-yellow-400'> ~/OneDrive/Documents/Web dev/builder-tut/client</span> <span className='text-blue-500'>(main)</span>
         </h1>
         <code className='text-green-700 pt-20 m-5'>
-          <span className='text-green-700'> $ {text}</span>
-          <span className='text-white'> <Cursor cursorStyle='<' /> </span>
+          <span className='text-white'> $ {text}</span>
+          <span className='text-green-700'> <Cursor cursorStyle='<' /> </span>
         </code>
 
       </div>
